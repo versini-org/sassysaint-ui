169a748305be9dc92960566478988be75623260d
Arno V <versini@gmail.com>
2023-08-23 20:13:36 -0400

    feat: nicer title font

--- packages/client/src/components/Messages/MessagesContainerHeader.tsx
+++ packages/client/src/components/Messages/MessagesContainerHeader.tsx
@@ -7,8 +7,10 @@ export const MessagesContainerHeader = () => {
 				<IconDog />
 			</div>
 			<div>
-				<h1 className="text-2xl font-bold text-slate-300">Sassy Saint</h1>
-				<h2 className="text-slate-300">ASK! ME! ANYTHING!</h2>
+				<h1 className="text-2xl md:text-3xl font-bold text-slate-300">
+					Sassy Saint
+				</h1>
+				<h2 className="text-slate-300 md:text-xl">ASK! ME! ANYTHING!</h2>
 			</div>
 		</div>
 	);
