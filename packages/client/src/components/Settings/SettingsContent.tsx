d4ae7672e86a94e8b2f714d98117a8d450aadcaf
Arno V <versini@gmail.com>
2023-09-03 18:14:22 -0400

    fix: profile panel crashes with new data

--- packages/client/src/components/Settings/SettingsContent.tsx
+++ packages/client/src/components/Settings/SettingsContent.tsx
@@ -1,5 +1,10 @@
 import { useContext } from "react";
 
+import {
+	DEFAULT_MODEL,
+	GTP3_MAX_TOKENS,
+	GTP4_MAX_TOKENS,
+} from "../../common/constants";
 import { Button } from "..";
 import { MessagesContext } from "../Messages/MessagesContext";
 
@@ -17,32 +22,35 @@ export const SettingsContent = ({
 	user,
 }: SettingsContentProps) => {
 	const { state } = useContext(MessagesContext);
-
 	const endUser = isDev ? { name: "ArnoDev", email: "toto@titi.fr" } : user;
-
 	const endState =
 		state && state.length > 0 && state[state.length - 1]
 			? state[state.length - 1]
-			: { model: "N/A", usage: "N/A" };
+			: { model: DEFAULT_MODEL, usage: 0 };
 
-	if (endState?.model?.includes("3.5") && !endState?.usage?.includes("N/A")) {
-		endState.usage = `${endState.usage} / 4096`;
-	}
-	if (endState?.model?.includes("4") && !endState?.usage?.includes("N/A")) {
-		endState.usage = `${endState.usage} / 8192`;
+	let remainingTokens = GTP3_MAX_TOKENS;
+
+	if (typeof endState?.usage === "number") {
+		if (endState?.model?.includes("4")) {
+			remainingTokens = GTP4_MAX_TOKENS - Number(endState.usage);
+		} else {
+			remainingTokens = GTP3_MAX_TOKENS - Number(endState.usage);
+		}
 	}
 
 	return (isAuthenticated && endUser) || isDev ? (
 		<div className="grid grid-flow-col justify-stretch gap-x-1">
-			<div className="border-slate-800 border-2 rounded-md p-4">
-				<h2 className="font-bold text-lg mb-2">User information</h2>
+			<div className="border-slate-900 border-2 rounded-md p-4 bg-slate-900 text-slate-200">
+				<h2 className="font-bold text-lg mb-4">User information</h2>
 				<dl>
-					<dt className="font-bold">Name</dt>
-					<dd className="mb-2">{endUser.name}</dd>
-					<dt className="font-bold">Email</dt>
-					<dd className="mb-2">{endUser.email}</dd>
+					<dt className="font-bold text-slate-400">Name</dt>
+					<dd className="mb-4">{endUser.name}</dd>
+					<dt className="font-bold text-slate-400">Email</dt>
+					<dd className="mb-4">{endUser.email}</dd>
 
 					<Button
+						kind="light"
+						fullWidth
 						slim
 						disabled={isDev}
 						className="mt-5"
@@ -53,13 +61,13 @@ export const SettingsContent = ({
 				</dl>
 			</div>
 
-			<div className="border-slate-800 border-2 rounded-md p-4">
-				<h2 className="font-bold text-lg mb-2">Real time statistics</h2>
+			<div className="border-slate-900 border-2 rounded-md p-4 bg-slate-900 text-slate-200">
+				<h2 className="font-bold text-lg mb-4">Real time statistics</h2>
 				<dl>
-					<dt className="font-bold">GTP model</dt>
-					<dd className="mb-2">{endState.model}</dd>
-					<dt className="font-bold">Usage</dt>
-					<dd className="mb-2">{endState.usage}</dd>
+					<dt className="font-bold text-slate-400">GTP model</dt>
+					<dd className="mb-4">{endState.model}</dd>
+					<dt className="font-bold text-slate-400">Remaining tokens</dt>
+					<dd className="mb-4">{remainingTokens}</dd>
 				</dl>
 			</div>
 		</div>
