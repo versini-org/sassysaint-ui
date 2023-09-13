1466f56cbd3f7f8c28dac4949310c641ce5c98eb
Arno V <versini@gmail.com>
2023-09-13 19:55:26 -0400

    fix: overlay background not black at lower bp

--- packages/client/src/components/Profile/Profile.tsx
+++ packages/client/src/components/Profile/Profile.tsx
@@ -30,10 +30,7 @@ export const Profile = ({
 
 	return (
 		<Dialog open={open} onOpenChange={onOpenChange}>
-			<DialogContent
-				overlayBackground="bg-slate-500 sm:bg-black/[.8]"
-				className="flex flex-col w-full sm:w-[95%] md:max-w-2xl min-h-[90%] sm:min-h-[10rem] rounded-lg border border-slate-200/10 bg-slate-400"
-			>
+			<DialogContent className="flex flex-col w-full sm:w-[95%] md:max-w-2xl min-h-[90%] sm:min-h-[10rem] rounded-lg border border-slate-200/10 bg-slate-400">
 				<DialogHeading className="p-4 text-xl font-bold flex flex-row-reverse justify-between">
 					<DialogClose>
 						<IconClose />
