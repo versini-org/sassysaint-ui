0a9b8f4559b21c1f862fb0b4f38ba4b6d24894a1
Arno V <versini@gmail.com>
2023-09-18 18:56:58 -0400

    Update utilities.ts

--- packages/client/src/common/utilities.ts
+++ packages/client/src/common/utilities.ts
@@ -121,16 +121,14 @@ export const obfuscate = (str: string) => {
 	 * UTF-8, then we convert the percent encodings into raw
 	 * bytes which can be fed into btoa.
 	 */
-	return typeof str === "string"
-		? window.btoa(
-				encodeURIComponent(str).replace(
-					/%([0-9A-F]{2})/g,
-					function toSolidBytes(_match, p1) {
-						return String.fromCharCode(Number(`0x${p1}`));
-					},
-				),
-		  )
-		: null;
+	return window.btoa(
+		encodeURIComponent(str).replace(
+			/%([0-9A-F]{2})/g,
+			function toSolidBytes(_match, p1) {
+				return String.fromCharCode(Number(`0x${p1}`));
+			},
+		),
+	);
 };
 
 export const unObfuscate = (str: string) => {
@@ -138,15 +136,13 @@ export const unObfuscate = (str: string) => {
 	 * Going backwards: from bytestream, to percent-encoding,
 	 * to original string.
 	 */
-	return typeof str === "string"
-		? decodeURIComponent(
-				window
-					.atob(str)
-					.split("")
-					.map(function (c) {
-						return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
-					})
-					.join(""),
-		  )
-		: null;
+	return decodeURIComponent(
+		window
+			.atob(str)
+			.split("")
+			.map(function (c) {
+				return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+			})
+			.join(""),
+	);
 };
