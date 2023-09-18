58c60aa7b002e3cd895d62dcf2b35080324aecf4
Arno V <versini@gmail.com>
2023-09-17 20:21:15 -0400

    reactor: extract reducer for easier testing

--- packages/client/src/modules/App.tsx
+++ packages/client/src/modules/App.tsx
@@ -4,10 +4,6 @@ import { v4 as uuidv4 } from "uuid";
 
 import {
 	ACTION_LOCATION,
-	ACTION_MESSAGE,
-	ACTION_MODEL,
-	ACTION_RESET,
-	ACTION_RESTORE,
 	DEFAULT_MODEL,
 	LOCAL_STORAGE_MODEL,
 } from "../common/constants";
@@ -15,93 +11,7 @@ import { useLocalStorage } from "../common/hooks";
 import { getCurrentGeoLocation, isDev } from "../common/utilities";
 import { Footer, Main, MessagesContainer } from "../components";
 import { AppContext } from "./AppContext";
-import { ActionProps, StateProps } from "./AppTypes";
-
-const reducer = (state: StateProps, action: ActionProps) => {
-	if (action.type === ACTION_RESTORE) {
-		const messages = action.payload.messages.map((item: any) => {
-			return {
-				message: {
-					role: item.role,
-					content: item.content,
-					name: item.name,
-				},
-			};
-		});
-		return {
-			id: action.payload.id,
-			model: action.payload.model,
-			usage: action.payload.usage,
-			location: state.location,
-			messages,
-		};
-	}
-
-	if (action.type === ACTION_MESSAGE) {
-		const role = action.payload.message.role;
-		const content = action.payload.message.content;
-		const name = action.payload.message.name;
-
-		if (role !== "" && content !== "") {
-			const message =
-				name && name !== ""
-					? {
-							role,
-							content,
-							name,
-					  }
-					: {
-							role,
-							content,
-					  };
-
-			return {
-				id: state.id,
-				model: state.model,
-				usage: state.usage,
-				location: state.location,
-				messages: [
-					...state.messages,
-					{
-						message,
-					},
-				],
-			};
-		}
-	}
-
-	if (action.type === ACTION_RESET) {
-		return {
-			id: uuidv4(),
-			model: state.model || DEFAULT_MODEL,
-			usage: 0,
-			messages: [],
-			location: state.location,
-		};
-	}
-
-	if (action.type === ACTION_MODEL) {
-		return {
-			id: state.id,
-			model: action.payload.model || state.model,
-			usage: action.payload.usage || state.usage,
-			messages: state.messages,
-			location: state.location,
-		};
-	}
-
-	if (action.type === ACTION_LOCATION) {
-		return {
-			id: state.id,
-			model: state.model,
-			usage: state.usage,
-			messages: state.messages,
-			location: action.payload.location,
-		};
-	}
-
-	return state;
-};
+import { reducer } from "./reducer";
 
 function App() {
 	const { isLoading } = useAuth0();
