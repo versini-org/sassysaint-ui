6cfc7ee658fc2750acfa22769b50b8f7e0ad5517
Arno V <versini@gmail.com>
2023-09-25 17:17:24 -0400

    wip

---
+++ packages/client/bundlemon.config.cjs
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+module.exports = {
+	baseDir: "./dist",
+	defaultCompression: "gzip",
+	files: [
+		{
+			path: "index.html",
+			maxSize: "2kb",
+			maxPercentIncrease: 5,
+		},
+		{
+			path: "assets/index-<hash>.js",
+			maxSize: "140kb",
+		},
+		{
+			path: "assets/index-<hash>.css",
+			maxSize: "10kb",
+		},
+		{
+			path: "assets/**/*.{png,svg}",
+		},
+	],
+};
