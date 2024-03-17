809e5956a251b9d774c84692d8e25816104a2d2c
Arno V <versini@gmail.com>
2024-03-17 18:14:02 -0400

    fix: better headings next to the logo

--- packages/client/src/modules/Messages/MessagesContainerHeader.tsx
+++ packages/client/src/modules/Messages/MessagesContainerHeader.tsx
@@ -228,11 +228,9 @@ export const MessagesContainerHeader = () => {
 				<div className="basis-1/4">
 					<IconDog />
 				</div>
-				<div>
-					<h1 className="heading text-2xl font-bold text-slate-300 md:text-3xl">
-						{APP_NAME}
-					</h1>
-					<h2 className="text-slate-300 md:text-xl">{APP_MOTTO}</h2>
+				<div className="prose prose-sm prose-light md:prose-base prose-h1:mb-0 prose-h2:mt-0">
+					<h1>{APP_NAME}</h1>
+					<h2>{APP_MOTTO}</h2>
 				</div>
 			</div>
 		</>
