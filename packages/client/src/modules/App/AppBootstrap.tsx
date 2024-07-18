301e051fa6303954e0bb999d6b118332a88d31f4
Arno V <versini@gmail.com>
2024-07-18 12:46:59 -0400

    fix: adding debug support for auth

--- packages/client/src/modules/App/AppBootstrap.tsx
+++ packages/client/src/modules/App/AppBootstrap.tsx
@@ -6,6 +6,10 @@ import { Suspense, lazy } from "react";
 import { CLIENT_ID } from "../../common/constants";
 import { DOMAIN } from "../../common/utilities";
 import { Login } from "../../modules/Login/Login";
+
+const params = new URL(document.location.href).searchParams;
+const debug = Boolean(params.get("debug")) || false;
+
 const LazyApp = lazy(() => import("./App"));
 
 const Bootstrap = ({ isComponent }: { isComponent: boolean }) => {
@@ -27,7 +31,7 @@ export const AppBootstrap = ({
 }: { isComponent: boolean; domain: string }) => {
 	return (
 		<>
-			<AuthProvider clientId={CLIENT_ID} domain={domain}>
+			<AuthProvider clientId={CLIENT_ID} domain={domain} debug={debug}>
 				<Bootstrap isComponent={isComponent} />
 			</AuthProvider>
 		</>
