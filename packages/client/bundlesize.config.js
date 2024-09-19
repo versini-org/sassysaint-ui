959fad18af3ae87c6db4e815c2f9137e8f3a35d0
Arno V <versini@gmail.com>
2024-09-19 18:18:57 -0400

    update

--- packages/client/bundlesize.config.js
+++ packages/client/bundlesize.config.js
@@ -4,63 +4,17 @@ export default {
 		current: "tmp/stats.json",
 	},
 	sizes: [
-		/**
-		 * JavaScript static assets.
-		 */
-		{
-			path: "dist/static/js/index.<hash>.js",
-			limit: "7 kb",
-		},
-		{
-			path: "dist/static/js/*versini_auth-provider*.<hash>.js",
-			limit: "28 kb",
-		},
-		{
-			path: "dist/static/js/lib-react.<hash>.js",
-			limit: "45 kb",
-		},
-		/**
-		 * JavaScript static async assets.
-		 */
-		{
-			path: "dist/static/js/async/*Messages_LazyHeader*.<hash>.js",
-			limit: "6 kb",
-		},
-		{
-			path: "dist/static/js/async/node_modules*versini_ui-components*.<hash>.js",
-			limit: "5 kb",
-		},
-		{
-			path: "dist/static/js/async/*App_App*.<hash>.js",
-			limit: "8 kb",
-		},
-		{
-			path: "dist/static/js/async/*react-use*.<hash>.js",
-			limit: "32 kb",
-		},
-		{
-			path: "dist/static/js/async/*katex*.<hash>.js",
-			limit: "51 kb",
-		},
-		{
-			path: "dist/static/js/async/*rehype-highlight*.<hash>.js",
-			limit: "130 kb",
-		},
-
-		/**
-		 * CSS static assets.
-		 */
-		{
-			path: "dist/static/css/index.<hash>.css",
-			limit: "11 kb",
-		},
-
-		/**
-		 * CSS static async assets.
-		 */
-		{
-			path: "dist/static/css/async/vendors-*katex*.<hash>.css",
-			limit: "8 kb",
-		},
+		{ path: "dist/static/js/async/574.1139ae94.js", limit: "1 kb" },
+		{ path: "dist/static/js/async/59.9ccec9e5.js", limit: "1 kb" },
+		{ path: "dist/static/js/async/693.c09c46fd.js", limit: "3 kb" },
+		{ path: "dist/static/js/async/918.d0b0ad54.js", limit: "5 kb" },
+		{ path: "dist/static/js/async/208.f18ea063.js", limit: "9 kb" },
+		{ path: "dist/static/js/async/939.43c6d13d.js", limit: "45 kb" },
+		{ path: "dist/static/js/async/44.9ea579bc.js", limit: "128 kb" },
+		{ path: "dist/static/js/index.1a69a1d3.js", limit: "6 kb" },
+		{ path: "dist/static/js/lib-react.16783681.js", limit: "46 kb" },
+		{ path: "dist/static/js/383.a435d4d1.js", limit: "53 kb" },
+		{ path: "dist/static/css/async/939.408bbacb.css", limit: "9 kb" },
+		{ path: "dist/static/css/index.01ecc885.css", limit: "11 kb" },
 	],
 };
