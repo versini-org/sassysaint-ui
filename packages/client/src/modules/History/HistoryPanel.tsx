2b129e44e16389abdc74f6e58252b95e3f49d6be
Arno V <versini@gmail.com>
2024-12-28 10:00:41 -0500

    feat: minor optimization in search history panel

--- packages/client/src/modules/History/HistoryPanel.tsx
+++ packages/client/src/modules/History/HistoryPanel.tsx
@@ -3,18 +3,16 @@ import { Panel } from "@versini/ui-panel";
 import { HISTORY_TITLE } from "../../common/strings";
 import { HistoryContent } from "./HistoryContent";
 
-export const History = ({
+export const HistoryPanel = ({
 	open,
 	onOpenChange,
-	historyData,
 }: {
-	historyData: any[];
 	onOpenChange: any;
 	open: boolean;
 }) => {
 	return (
 		<Panel open={open} onOpenChange={onOpenChange} title={HISTORY_TITLE}>
-			<HistoryContent onOpenChange={onOpenChange} historyData={historyData} />
+			<HistoryContent onOpenChange={onOpenChange} />
 		</Panel>
 	);
 };
