52fca56282ac1e9d95378aaad95d58fc5b54bfbb
Arno V <versini@gmail.com>
2024-12-30 13:23:54 -0500

    feat: adding capability to copy code snippets to clipboard

--- packages/client/src/modules/Messages/LazyMarkdownWithExtra.tsx
+++ packages/client/src/modules/Messages/LazyMarkdownWithExtra.tsx
@@ -1,18 +1,57 @@
 import "katex/dist/katex.min.css";
 
+import { ButtonCopy } from "@versini/ui-button";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+import { visit } from "unist-util-visit";
+
+interface PreNode {
+	node?: any;
+	position: object;
+	properties: object;
+	tagName: string;
+	type: string;
+}
+
+const captureRawCode = () => (tree: any) => {
+	visit(tree, "element", (node: any) => {
+		if (node.tagName === "code" && node.children?.[0]?.value) {
+			node.properties["data-raw-code"] = node.children[0].value;
+		}
+	});
+};
 
 const LazyReactMarkdownWithExtra = ({
 	content,
 }: { content: string | undefined }) => {
+	const handleCopyClick = (pre: any): string => {
+		return pre.children.props["data-raw-code"] || "";
+	};
+
 	return (
 		<ReactMarkdown
+			components={{
+				pre: (node: PreNode) => {
+					return (
+						<div className="flex flex-row gap-2">
+							<pre
+								{...node}
+								style={{ marginTop: 0, marginBottom: 0 }}
+								className="text-xs"
+							></pre>
+							{/* The extra div is needed to prevent flex from squashing the button */}
+							<div>
+								<ButtonCopy copyToClipboard={() => handleCopyClick(node)} />
+							</div>
+						</div>
+					);
+				},
+			}}
 			remarkPlugins={[remarkGfm, remarkMath]}
-			rehypePlugins={[rehypeHighlight, rehypeKatex]}
+			rehypePlugins={[captureRawCode, rehypeHighlight, rehypeKatex]}
 		>
 			{content}
 		</ReactMarkdown>
