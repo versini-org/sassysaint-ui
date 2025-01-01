71f68b5404d4e78fd86b1edf68de473555b2cb9d
Arno V <versini@gmail.com>
2024-12-31 20:22:54 -0500

    feat: sticky header + new chat trigger in header too

--- packages/client/src/modules/Messages/LazyHeader.tsx
+++ packages/client/src/modules/Messages/LazyHeader.tsx
@@ -15,6 +15,7 @@ import { Menu, MenuItem, MenuSeparator } from "@versini/ui-menu";
 import { ToggleGroup, ToggleGroupItem } from "@versini/ui-togglegroup";
 import { useContext, useEffect, useState } from "react";
 
+import { Flexgrid, FlexgridItem } from "@versini/ui-system";
 import {
 	ACTION_ENGINE,
 	ACTION_RESET,
@@ -29,6 +30,7 @@ import { About } from "../About/About";
 import { AppContext } from "../App/AppContext";
 import { ChatDetails } from "../ChatDetails/ChatDetails";
 import { ConfirmationPanel } from "../Common/ConfirmationPanel";
+import { NewChatButton } from "../Common/NewChatButton";
 import { HistoryPanel } from "../History/HistoryPanel";
 import { Profile } from "../Profile/Profile";
 
@@ -90,113 +92,123 @@ const LazyHeader = () => {
 			<ChatDetails open={showChatDetails} onOpenChange={setShowChatDetails} />
 			<HistoryPanel open={showHistory} onOpenChange={setShowHistory} />
 			<About open={showAbout} onOpenChange={setShowAbout} />
-			<div className="relative">
-				{showEngineToggleInMenu && serverStats && (
-					<div className="absolute bottom-[-28px] left-[-7px]">
-						<Menu
-							mode="dark"
-							focusMode="light"
-							trigger={
-								<ButtonIcon>
-									{state && state.engine === ENGINE_ANTHROPIC ? (
-										<IconAnthropic />
-									) : (
-										<IconOpenAI />
-									)}
-								</ButtonIcon>
-							}
-							defaultPlacement="bottom-start"
-						>
-							<MenuItem raw ignoreClick>
-								<ToggleGroup
-									size="small"
+
+			<div className="sticky top-0 bg-slate-900 py-4">
+				<Flexgrid alignHorizontal="space-between" alignVertical="center">
+					<FlexgridItem>
+						<NewChatButton mode="dark" radius="large" />
+					</FlexgridItem>
+
+					<FlexgridItem>
+						<Flexgrid>
+							{showEngineToggleInMenu && serverStats && (
+								<FlexgridItem>
+									<Menu
+										mode="dark"
+										focusMode="light"
+										trigger={
+											<ButtonIcon className="mr-2">
+												{state && state.engine === ENGINE_ANTHROPIC ? (
+													<IconAnthropic />
+												) : (
+													<IconOpenAI />
+												)}
+											</ButtonIcon>
+										}
+										defaultPlacement="bottom-start"
+									>
+										<MenuItem raw ignoreClick>
+											<ToggleGroup
+												size="small"
+												mode="dark"
+												focusMode="light"
+												value={engine}
+												onValueChange={async (value: string) => {
+													if (value) {
+														try {
+															await serviceCall({
+																accessToken: await getAccessToken(),
+																type: SERVICE_TYPES.SET_USER_PREFERENCES,
+																params: {
+																	user: user?.username,
+																	engine: value,
+																},
+															});
+															dispatch({
+																type: ACTION_ENGINE,
+																payload: {
+																	engine: value,
+																},
+															});
+															dispatch({
+																type: ACTION_RESET,
+															});
+														} catch (_error) {
+															// nothing to declare officer
+														}
+													}
+												}}
+											>
+												{serverStats &&
+													serverStats.engines.map((engine) => (
+														<ToggleGroupItem key={engine} value={engine} />
+													))}
+											</ToggleGroup>
+										</MenuItem>
+									</Menu>
+								</FlexgridItem>
+							)}
+							<FlexgridItem>
+								<Menu
 									mode="dark"
 									focusMode="light"
-									value={engine}
-									onValueChange={async (value: string) => {
-										if (value) {
-											try {
-												await serviceCall({
-													accessToken: await getAccessToken(),
-													type: SERVICE_TYPES.SET_USER_PREFERENCES,
-													params: {
-														user: user?.username,
-														engine: value,
-													},
-												});
-												dispatch({
-													type: ACTION_ENGINE,
-													payload: {
-														engine: value,
-													},
-												});
-												dispatch({
-													type: ACTION_RESET,
-												});
-											} catch (_error) {
-												// nothing to declare officer
-											}
-										}
-									}}
-								>
-									{serverStats &&
-										serverStats.engines.map((engine) => (
-											<ToggleGroupItem key={engine} value={engine} />
-										))}
-								</ToggleGroup>
-							</MenuItem>
-						</Menu>
-					</div>
-				)}
-
-				<div className="absolute bottom-[-28px] right-[-7px]">
-					<Menu
-						mode="dark"
-						focusMode="light"
-						trigger={
-							<ButtonIcon>
-								<IconSettings />
-							</ButtonIcon>
-						}
-						defaultPlacement="bottom-end"
-					>
-						<MenuItem
-							label="Profile"
-							onClick={onClickProfile}
-							icon={<IconProfile />}
-						/>
-						<MenuItem
-							label={STATS}
-							onClick={onClickChatDetails}
-							icon={<IconChart />}
-						/>
-						<MenuItem
-							label="History"
-							onClick={onClickHistory}
-							icon={<IconHistory />}
-						/>
-						<MenuItem
-							label="About"
-							onClick={onClickAbout}
-							icon={<IconInfo />}
-						/>
-
-						{state && state.id && !state.isComponent && (
-							<>
-								<MenuSeparator />
-								<MenuItem
-									label="Log out"
-									onClick={onClickConfirmLogout}
-									icon={
-										<div className="text-red-700">
-											<IconBack monotone />
-										</div>
+									trigger={
+										<ButtonIcon>
+											<IconSettings />
+										</ButtonIcon>
 									}
-								/>
-							</>
-						)}
-					</Menu>
-				</div>
+									defaultPlacement="bottom-end"
+								>
+									<MenuItem
+										label="Profile"
+										onClick={onClickProfile}
+										icon={<IconProfile />}
+									/>
+									<MenuItem
+										label={STATS}
+										onClick={onClickChatDetails}
+										icon={<IconChart />}
+									/>
+									<MenuItem
+										label="History"
+										onClick={onClickHistory}
+										icon={<IconHistory />}
+									/>
+									<MenuItem
+										label="About"
+										onClick={onClickAbout}
+										icon={<IconInfo />}
+									/>
+
+									{state && state.id && !state.isComponent && (
+										<>
+											<MenuSeparator />
+											<MenuItem
+												label="Log out"
+												onClick={onClickConfirmLogout}
+												icon={
+													<div className="text-red-700">
+														<IconBack monotone />
+													</div>
+												}
+											/>
+										</>
+									)}
+								</Menu>
+							</FlexgridItem>
+						</Flexgrid>
+					</FlexgridItem>
+				</Flexgrid>
 			</div>
 		</>
 	);
